a80f3103e5156b9975b6505f3e20e2bfa9bd86a8
Julien Afonso <ju.afonso61@gmail.com>
2023-06-20 00:45:07 +0900

    practice

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,20 @@
 class PagesController < ApplicationController
-
   def home
   end
 
   def dinosaur
+    @dinos = ['tyrannosaurus', 'stegosaurus', 'spinosaurus', 'triceratops', 'allosaurus', 'velociraptor', 'brachiosaurus', 'ankylosaurus' ]
 
+    if params[:dino]
+      @dinos = @dinos.select { |dino| dino.start_with?(params[:dino]) }
+    end
   end
 
   def animal
-  end
+    @members = ["thanh", "dimitri", "germain", "damien", "julien"]
 
+    if params[:member]
+      @members = @members.select { |member| member.start_with?(params[:member]) }
+    end
+  end
 end
